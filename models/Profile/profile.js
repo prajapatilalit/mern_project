9e14de5f28a640d39313ad40017811218ce122cb
prajapatilalit <prajapatilalit150992@gmail.com>
2021-03-18 12:24:54 +0530

    profile model update

---
+++ models/Profile/profile.js
@@ -0,0 +1,112 @@
+const mongoose = require("mongoose");
+const profileSchema = new mongoose.Schema({
+  User: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    require: true,
+  },
+  skills: {
+    type: [String],
+    require: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        require: true,
+      },
+      company: {
+        type: String,
+        require: true,
+      },
+      location: {
+        type: String,
+      },
+      from: {
+        type: Date,
+        require: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        require: true,
+      },
+      degree: {
+        type: String,
+        require: true,
+      },
+      fieldofstudy: {
+        type: String,
+        require: true,
+      },
+      from: {
+        type: Date,
+        require: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twiter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkdin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model("profile", profileSchema);
--- routers/api/profile.js
+++ routers/api/profile.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/Users/user");
+const Profile = require("../../models/Profile/profile");
 
-//@route    GET api/profile
-//@desc     Test route
-//@access   public
+//@route    GET api/profile/me
+//@desc     Get current user profile
+//@access   Private
 
-router.get("/", (req, res) => res.send("profile route"));
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profil of this user" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send("sever error");
+  }
+});
 
 module.exports = router;
