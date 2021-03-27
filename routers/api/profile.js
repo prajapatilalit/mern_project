12522f98f9b293c771ff63fc56fecf1d178c9544
prajapatilalit <prajapatilalit150992@gmail.com>
2021-03-28 00:32:29 +0530

    update route of get profile by user id

--- routers/api/profile.js
+++ routers/api/profile.js
@@ -100,8 +100,41 @@ router.post(
       res.json(profile);
     } catch (err) {
       console.error(err.message);
-      return res.status(500).send("Server error");
+      return res.status(500).send("Server Error");
     }
   }
 );
+
+//@route    GET api/profile
+//@desc     Get all user profile
+//@access   Public
+
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send("Server Error");
+  }
+});
+
+//@route    GET api/profile/user/:user_id
+//@desc     Get Profile by user id
+//@access   Public
+
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) {
+      res.status(400).send({ msg: "there is no profile for this user" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
