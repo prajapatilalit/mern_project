35cf0a2585aa8a7c779e4f96de10b2ae3e197d65
prajapatilalit <prajapatilalit150992@gmail.com>
2021-04-02 14:08:09 +0530

    create and update alert component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Login from "./components/auth/login/Login";
 import Register from "./components/auth/register/Register";
 import Landing from "./components/layout/landing/Landing";
 import Navbar from "./components/layout/navbar/Navbar";
+import Alert from "./components/layout/alert/Alert";
 
 //Redux
 import { Provider } from "react-redux";
@@ -18,6 +19,7 @@ const App = () => {
           <Navbar />
           <Route exact path="/" component={Landing} />
           <section className="container">
+            <Alert />
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
