6baab34b9c2c072c51e31530f07733248804a263
prajapatilalit <prajapatilalit150992@gmail.com>
2021-05-07 00:17:25 +0530

    update the posts reducer and action and component

---
+++ client/src/components/posts/Posts.js
@@ -0,0 +1,22 @@
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layout/spinner/Spinner";
+import { getPosts } from "../../action/post";
+const Posts = ({ getPosts, post: { posts, loading } }) => {
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+  return loading ? <Spinner /> : <Fragment>test</Fragment>;
+};
+
+Posts.propTypes = {
+  getPosts: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
+
+export default connect(mapStateToProps, { getPosts })(Posts);
---
+++ client/src/reducers/post.js
@@ -0,0 +1,31 @@
+import { GET_POST, POST_ERROR } from "../action/types";
+
+const initialState = {
+  posts: [],
+  post: null,
+  loading: false,
+  error: {},
+};
+
+export default function post(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case GET_POSTS: {
+      return {
+        ...state,
+        post: payload,
+        loading: false,
+      };
+    }
+    case POST_ERROR: {
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
