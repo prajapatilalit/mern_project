263e6cba26f7b8c05055641153407e2f56240252
prajapatilalit <prajapatilalit150992@gmail.com>
2021-05-06 17:53:45 +0530

    update education and experience section in profile

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,13 @@ import Landing from "./components/layout/landing/Landing";
 import Navbar from "./components/layout/navbar/Navbar";
 import Alert from "./components/layout/alert/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
-import PrivateRoute from "./components/routing/PrivateRoute";
 import CreateProfile from "./components/profile-form/CreateProfile";
 import EditProfile from "./components/profile-form/EditProfile";
 import AddExperience from "./components/profile-form/AddExperience";
 import AddEducation from "./components/profile-form/AddEducation";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
+import PrivateRoute from "./components/routing/PrivateRoute";
 //Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -40,6 +41,7 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
